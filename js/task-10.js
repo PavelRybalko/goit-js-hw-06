39c4be0887636a7af76045cb27eaa51d0df9278d
Pavel Rybalko <freedom277f@gmail.com>
2020-08-07 17:55:25 +0300

    fix task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,16 +1,10 @@
 import users from "./users.js";
 
-const getSortedUniqueSkills = (users) =>
-  users
-    .reduce((acc, user) => {
-      user.skills.forEach((skill) => {
-        if (!acc.includes(skill)) {
-          acc.push(skill);
-        }
-      });
-
-      return acc;
-    }, [])
-    .sort();
+const getSortedUniqueSkills = users 
+=>
+    users.reduce((acc, user) =>
+        [...user.skills, ...acc], [])
+           .filter((itm, idx, arr) => arr.indexOf(itm) === idx)
+        .sort();
 
 console.log(getSortedUniqueSkills(users));
